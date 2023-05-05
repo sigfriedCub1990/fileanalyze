085e07c183b2dd2f7203e7d5611d6b4d0986bb11
Ruben S. Garcia <rssanjuan2704@gmail.com>
2023-05-04 19:02:20 -0600

    feat: Create and configure basic view

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,17 @@ dotenv.config();
 
 const app = express();
 
+app.use(morgan("dev"));
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set("views", path.join(__dirname, "../../src/views"));
+app.set("view engine", "pug");
 
 app.get("/", (_req: Request, res: Response) => {
-  res.json({
-    message: "Hello world",
-  });
+  res.render("index");
 });
 
 app.get("/api", (_, res: Response) => {
